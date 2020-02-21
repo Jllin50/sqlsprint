607d636d050a21fd1fe52a5b93f1c457c7ea5c79
Jllin50 <31583768+Jllin50@users.noreply.github.com>
2020-02-21 11:02:17 -0800

    Add files via upload

---
+++ northwind.py
@@ -0,0 +1,38 @@
+import sqlite3
+import pandas as pd
+
+DB_FILEPATH = 'northwind_small_plural.sqlite3'
+connection = sqlite3.connect(DB_FILEPATH)
+cursor = connection.cursor()
+
+#Top 10 highest priced products
+
+query = '''SELECT ProductName, UnitPrice FROM Products
+  ORDER BY UnitPrice DESC
+  LIMIT 10'''
+result = cursor.execute(query).fetchall()
+print(f'The top 10 highest priced products and their price: \n {result}\n')
+
+# Average age at hire date
+
+query = '''SELECT avg(HireDate - BirthDate) FROM Employees'''
+result = cursor.execute(query).fetchall()
+print(f'Average Age at Hire Date: \n {result[0][0]}\n')
+
+# - What are the ten most expensive items (per unit price) in the database *and* their suppliers
+
+query = '''SELECT ProductName, CompanyName FROM Products, Suppliers
+WHERE Products.SupplierID = Suppliers.SupplierID
+ORDER BY Products.UnitPrice DESC
+LIMIT 10'''
+result = cursor.execute(query).fetchall()
+print(f'Most expensive items and their suppliers: {result}\n')
+
+# - What is the largest category (by number of unique products in it)?
+
+query = '''SELECT CategoryName FROM Categories, Products
+GROUP BY Categories.CategoryID
+ORDER BY COUNT(DISTINCT Products.ProductID) DESC
+LIMIT 1'''
+result = cursor.execute(query).fetchall()
+print(f'Largest product category: {result[0][0]}')
